6dec166ef4653647f5075a624559c99ce7a53688
223961 Gryglewska Maria <223961@edu.p.lodz.pl>
2021-04-29 12:54:14 +0200

    [Mail Service] Init project, mail confirmation service

---
+++ MailingService/Domain/Services/MailSender.cs
@@ -0,0 +1,85 @@
+﻿using Domain.Exceptions;
+using Domain.Services.Contracts;
+using Domain.Settings;
+using FluentEmail.Core;
+using FluentEmail.Smtp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Domain.Services
+{
+    public class MailSender : IMailSender
+    {
+        private SmtpSender _Sender { get; set; }
+        private TemplatesSettings _TemplatesSettings { get; }
+        private SmtpSettings _SmtpSettings { get; }
+        public MailSender(TemplatesSettings templatesSettings, SmtpSettings smtpSettings)
+        {
+            _SmtpSettings = smtpSettings;
+            _TemplatesSettings = templatesSettings;
+            _Sender = new SmtpSender(() => new SmtpClient(_SmtpSettings.Host)
+            {
+                //EnableSsl = false,
+                //DeliveryMethod = SmtpDeliveryMethod.Network,
+                //Port = 25
+                EnableSsl = false,
+                DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory,
+                PickupDirectoryLocation = _SmtpSettings.TestFolder
+            });
+            Email.DefaultSender = _Sender;
+        }
+
+        public async Task SendConfirmationEmailAsync(string from, string to, string subject, string token)
+        {
+            try
+            {
+                var templatePath = _TemplatesSettings.ConfirmationFilePath;
+                var template = File.ReadAllText(templatePath);
+                var body = string.Format(template, token);
+
+                var sendResponse = await Email
+                    .From(from)
+                    .To(to)
+                    .Subject(subject)
+                    .Body(body, true)
+                    .SendAsync();
+
+                if (!sendResponse.Successful)
+                {
+                    throw new EmailException("Email was not send successfully", from, to);
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new EmailException("Email sender encountered unexpected error", exception, from, to);
+            }
+        }
+
+        public async Task SendEmailAsync(string from, string to, string subject, XDocument html)
+        {
+            try
+            {
+                var sendResponse = await Email
+                    .From(from)
+                    .To(to)
+                    .Subject(subject)
+                    .Body(html.ToString(), true)
+                    .SendAsync();
+
+                if (!sendResponse.Successful)
+                {
+                    throw new EmailException("Email was not send successfully", from, to);
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new EmailException("Email sender encountered unexpected error", exception, from, to);
+            }
+        }
+    }
+}
---
+++ MailingService/Domain/Settings/TemplatesSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Domain.Settings
+{
+    public class TemplatesSettings
+    {
+        public string ConfirmationFile { get; set; }
+        public string ConfirmationFilePath
+        {
+            get
+            {
+                return Path.Combine(BasePath, ConfirmationFile);
+            }
+        }
+        public string BasePath
+        {
+            get
+            {
+                var currentDirectory = Directory.GetCurrentDirectory();
+                return Path.Combine(currentDirectory, "EmailTemplates");
+            }
+        }
+    }
+}
